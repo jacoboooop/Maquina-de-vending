496127a59712dfa69387483b58812786436cc7be
jacoboooop <9301232@alumnos.ufv.es>
2024-04-08 12:36:05 +0200

    materiales preciosos

---
+++ Maquina de vending/Materiales_preciosos.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maquina_de_vending {
+    internal class Materiales_perciosos : Productos{
+
+    }
+}
