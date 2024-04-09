f823441ce8f81b7e8698350a3da1c12b0c31ae02
jacoboooop <9301232@alumnos.ufv.es>
2024-04-09 08:13:36 +0200

    materiales preciosos 1

--- Maquina de vending/Materiales_preciosos.cs
+++ Maquina de vending/Materiales_preciosos.cs
@@ -7,5 +7,14 @@ using System.Threading.Tasks;
 namespace Maquina_de_vending {
     internal class Materiales_perciosos : Productos{
 
+        public string Tipo {  get; set; }
+        public int Peso { get; set; }
+
+        public Materiales_perciosos() { }
+        public Materiales_perciosos(string nombre, int unidades, double precio_unitario, string descripcion, string tipo, int peso)
+            : base(nombre, unidades, precio_unitario, descripcion) {
+            Tipo = tipo;
+            Peso = peso;
+        }
     }
 }
