cd164b455ab82628a65ae07a9c00f13152fbb380
Geertprumm <geertprummel095@gmail.com>
2024-04-16 09:33:23 +0200

    Clase Productos Alimenticios

--- Maquina de vending/ProductosAlimenticios.cs
+++ Maquina de vending/ProductosAlimenticios.cs
@@ -8,6 +8,28 @@ namespace Maquina_de_vending
 {
     internal class ProductosAlimenticios : Productos
     {
-        
+        public double Calorias { get; set; }
+        public double  Grasas { get; set; }
+        public double Azucares { get; set; }
+
+
+
+        public ProductosAlimenticios() { }
+
+
+
+        public ProductosAlimenticios(double calorias, double grasas, double azucares)
+        {
+            Calorias = calorias;
+            Grasas = grasas;
+            Azucares = azucares;
+            
+        }
+
+
+        public override string MostrarInformcion()
+        {
+            return base.MostrarInformcion() + $"\nCalorias: {Calorias} \nGrasas: {Grasas} \nAzucares: {Azucares}";
+        }
     }
 }
