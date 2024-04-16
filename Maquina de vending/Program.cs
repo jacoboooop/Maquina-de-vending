a13680a7ef4a96343d24506c00427d80f81a8d21
Luiseesa <sluisete1204@gmail.com>
2024-04-16 09:41:06 +0200

    ComprarProductos

--- Maquina de vending/Program.cs
+++ Maquina de vending/Program.cs
@@ -32,7 +32,13 @@ namespace Maquina_de_vending {
 
         public static void ComprarProductos()
         {
-
+            Console.Clear();
+            Console.WriteLine("Introduzca el ID del producto que sea comprar\n\n");
+            foreach (Productos p in listaProductos)
+            {
+                Console.WriteLine($"{p.MostrarInformcion()}");
+            }
+            Console.Write("\n\nID: ");
         }
     }
 }
